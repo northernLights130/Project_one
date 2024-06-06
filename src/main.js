a99781b6964c5c6e1400d6a035d78c1bd6b9b81e
northernLights130 <curvus90@gmail.com>
2024-06-06 21:39:40 +0700

    change test1

---
+++ src/main.js
@@ -0,0 +1,9 @@
+window.addEventListener("load", function (){
+
+
+    let btn = document.querySelector("button");
+    btn.addEventListener("click", function (){
+        alert("Hello World!");
+    })
+
+})
