6a1491c9e77ddf24943689af658a630a79a4352c
northernLights130 <curvus90@gmail.com>
2024-06-06 21:48:28 +0700

    delete all

--- src/main.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("load", function (){
-
-
-    let btn = document.querySelector("button");
-    btn.addEventListener("click", function (){
-        alert("Hello World!");
-    })
-
-})
